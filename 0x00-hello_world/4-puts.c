51b25d8ebcaf8ed16780a7a6b3f02ee9ef9a45ff
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-13 11:07:42 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-	/**
-	 * main-entry point
-	 *
-	 * Return:always 0(success)
-	 */
-	int main(void)
+
+int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle ,\n");
-	return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle \n);
+	return(0);
+}	
