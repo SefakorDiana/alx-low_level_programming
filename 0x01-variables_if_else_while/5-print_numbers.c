7904623fbd86085a7ae44eef3086c5edaef11f14
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-14 10:18:46 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Printing alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+	printf('0' + number);
+	number++,
+	}
+	return (0);
+}
