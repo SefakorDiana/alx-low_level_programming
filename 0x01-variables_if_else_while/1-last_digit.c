7b23603566671c483c8b19db71fcea0fdc3938c3
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-14 07:26:33 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,13 +15,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is", n);
-	int last = n % 10;
 
-	if (last > 5)
+	if (n > 5)
 	{
 	printf(" and is greater than 5\n");
 	}
-	else if (last == 0)
+	else if (n == 0)
 	{
 	printf(" and is 0\n");
 	}
