5cd9aaefd39f7a579295eaccdd707d27bb66bb54
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-14 12:01:29 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Printing possible combinations of single-digit
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n')'
+	return (0);
+}
