1c03a56425fa0852db4539682dd07bb506afed14
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-14 10:54:18 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Printing alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= a)
+	{
+	putchar(letter);
+	letter--;
+	}
+	return (0);
+}
