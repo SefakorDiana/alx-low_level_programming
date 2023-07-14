9f287c6d54261edf7031be7bbbd661eba2971408
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-14 06:06:02 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d\n", n);
-	if (n > 0
+	if (n > 0)
 	{
 	printf("is positive\n");
 	}
@@ -27,6 +27,5 @@ int main(void)
 	{
 	printf("is negative\n");
 	}
-
 	return (0);
 }
