2cb74b1e2b999d1299327a28c149525e2924c88f
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-19 15:52:27 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - Determine if a number is positive, negative or zero
- * 0: is the number to be checked
+ * positive_or_negative - Determine if a number is positive, negative or zero
+ * @i: is the number to be checked
  * Return (0)
  */
 
@@ -19,5 +19,4 @@ void positive_or_negative(int i)
 	else
 	{printf("%d is %s\n", i, "zero");
 	}
-	return;
 }
