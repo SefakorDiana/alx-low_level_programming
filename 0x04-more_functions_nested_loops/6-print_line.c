4b72621d736ffebd9c656cb5eb3c31cd62d7730e
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-20 12:56:41 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,9 +12,13 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 	_putchar('-');
+	}
+	if (n == 0 && n <= 0)
+	{
+	putchar('\n');
 	}
 	putchar('\n');
 }
