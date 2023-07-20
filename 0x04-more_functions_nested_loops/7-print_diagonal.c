e3d4d567b6567b20afc87fb39b6db7140948a100
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-20 14:49:09 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,19 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < n; i++)
 	{
-	_putchar('\');
+	for (j = 0; j < i; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
 	}
 	if (i <= 0)
 	{
 	_putchar('\n');
 	}
-	_putchar('\n');
 }
