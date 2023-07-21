155943d6b3d0089226c7a17c05b972caae996432
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-21 12:36:16 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,16 +14,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	if (i % 15  == 0)
-		printf("FizzBuzz");
-	else if (i % 3 == 0)
-		printf("Fizz");
-	else if (i % 5 == 0)
-		printf("Buzz");
-	else
-		printf("%i", i);
-	if (i < 100)
-		printf(" ");
+		if (i % 15  == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
