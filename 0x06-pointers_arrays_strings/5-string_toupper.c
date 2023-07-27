097c527e9fb256f102d9e37baf215245b0066506
Diana Sefakor Acquah <dianaacquah8@gmail.com>
2023-07-27 06:55:15 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase
+ * letters of a string to uppercase
+ * @x: pointer to string
+ * Return: pointer to uppercase string
+ */
+
+char *string_toupper(char *x)
+{
+	int lenght_of_string;
+
+	lenght_of_string = 0;
+
+	while (x[lenght_of_string] != '\0')
+	{
+		if (x[lenght_of_string] >= 97 && x[lenght_of_string] <= 122)
+		{
+			x[lenght_of_string] = x[lenght_of_string] - 32;
+		}
+		lenght_of_string++;
+	}
+	return (x);
+}
